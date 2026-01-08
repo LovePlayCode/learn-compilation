a7ffbe39e0a70fe66996a437314bbcd028fe7bc4
LovePlayCode <1244224501@qq.com>
2026-01-08 17:26:55 +0800

    feat: 实现变量赋值解析与执行

--- src/com/craftinginterpreters/lox/Environment.java
+++ src/com/craftinginterpreters/lox/Environment.java
@@ -5,9 +5,38 @@ import java.util.Map;
 
 class Environment {
     private final Map<String, Object> values = new HashMap<>();
+    final Environment enclosing;
+
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     void define(String name, Object value) {
         values.put(name, value);
+    }
+
+    /**
+     * 赋值与定义的主要区别在于，赋值操作不允许创建新变量，如果环境的变量映射中不存在变量值，而使用了这个变量，就是一个运行时错误
+     * 
+     * @param name
+     * @param value
+     */
+    void assign(Token name, Object value) {
+        if (values.containsKey(name.lexeme)) {
+            values.put(name.lexeme, value);
+            return;
+        }
+        // 新增部分开始
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
+        throw new RuntimeError(name,
+                "Undefined variable '" + name.lexeme + "'.");
     }
 
     // 新增部分开始
@@ -15,7 +44,9 @@ class Environment {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
-
+        // 作用域链的概念，先查找本地环境，如果查不到的话，往上层环境查找。
+        if (enclosing != null)
+            return enclosing.get(name);
         throw new RuntimeError(name,
                 "Undefined variable '" + name.lexeme + "'.");
     }
